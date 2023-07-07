fd6b580086037deb606eaf10b299e28b972f213a
Zachary Ringwood <zringwood@gmail.com>
2023-07-07 11:34:41 -0400

    Server now responds with example data

--- convertToJSON.js
+++ convertToJSON.js
@@ -1,12 +1,7 @@
 //Temp file, used to convert txt data to JSON
-let fileName = "Mate en Dos.pgn";
+let fileName = "MateenDos.pgn"
+const games = require("./MateenDos.pgn")
 
-async function loadFile(){
-let file = await fetch("C:/Users/zacha/Documents/Bootcamp Files/pair-programming/Mate en Dos.pgn").then((response) => {
+if(games){
     console.log("Reached")
-}).catch((response) => {
-    console.log(response)
-})
-    return file
 }
-loadFile();
--- server/server.js
+++ server/server.js
@@ -5,20 +5,59 @@ const port = process.env.PORT || process.argv[2] || 8080;
 const { v4: uuidv4 } = require('uuid');
 // const cors = require('cors');
 
+const examplePuzzles = [{
+    id: "1",
+    Event: "Deutsche Schachzeitung",
+    Site: "?",
+    Date: "1968.03.??",
+    Round: "-",
+    White: "9 AUTHORS",
+    Black: "#2",
+    Result: "1-0",
+    FEN: "8/8/8/8/1Q6/1K6/8/2Nk4 w - - 0 1",
+    SetUp: "1",
+    Moves: ["1. Qa5 Kxc1", "2. Qe1# 1-0"]
+}, {
+    id: "2",
+    Event: "Illustrated London News",
+    Site: "?",
+    Date: "1849.??.??",
+    Round: "-",
+    White: "A. B. S.",
+    Black: "#2",
+    Result: "1-0",
+    FEN: "8/8/5R2/8/2P1k3/2K5/5P2/2B5 w - - 0 1",
+    SetUp: "1",
+
+    Moves: ["1. Bb2 Ke5", "2. Kd3# 1-0"]
+}, {
+    id: "3",
+    Event: "The British Chess Magazine",
+    Site: "?",
+    Date: "1882.??.??",
+    Round: "-",
+    White: "A. L. S.",
+    Black: "#2",
+    Result: "1-0",
+    FEN: "2b4Q/4p2K/4pr2/2P1k3/2P2R2/3P3p/2n4B/8 w - - 0 1",
+    SetUp: "1",
+    Moves: ["1. Kg7 Nd4 1-0"]
+}
+];
+
 app.get("/", (req, res) => {
-    res.sendStatus(200);
+    return res.sendStatus(200);
 })
-//This route takes in PGN data as a raw string and returns a JSON object containing the proper data. 
-app.post("/:pgn", (req, res) => {
-    pgn = req.params;
-    if(pgn)
-    return res.sendStatus(200)
-    return res.status(404).send("Invalid PGN")
+app.get("/:id", (req, res) => {
+    let id = req.params.id;
+   
+    let puzzle = examplePuzzles.find(element => {
+        return element.id === id
+    })
+    if(puzzle){
+        return res.status(200).send(puzzle);
+    } return res.status(404).send("No puzzle with that ID found");
 })
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
-//Takes a game in PGN notation and returns an object containing the PGN data. 
-function getGameState(pgn){
-
-}
